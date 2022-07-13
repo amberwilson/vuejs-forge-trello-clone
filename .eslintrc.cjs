43f0f1bb742461fc9fe23aaeb59aa84718590fda
Amber Wilson <amber@arborxr.com>
2022-07-13 11:46:34 -0300

    Allow single word component names

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,4 +15,8 @@ module.exports = {
       extends: ["plugin:cypress/recommended"],
     },
   ],
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
+  ignorePatterns: ["*.config.js"],
 };
